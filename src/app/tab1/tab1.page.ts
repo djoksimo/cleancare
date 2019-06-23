f818635fc4ef1762448254d8ce17ba2f8828cab8
Stefan Mitic <stefan.mitic@mail.utoronto.ca>
2019-06-23 02:02:21 -0400

    Fixed error with ios transition

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -118,13 +118,11 @@ export class Tab1Page {
     const hours = Math.floor(secNum / 3600);
     const minutes = Math.floor((secNum - (hours * 3600)) / 60);
     const seconds = secNum - (hours * 3600) - (minutes * 60);
-    let hoursString = '';
     let minutesString = '';
     let secondsString = '';
-    hoursString = (hours < 10) ? '0' + hours : hours.toString();
     minutesString = (minutes < 10) ? '0' + minutes : minutes.toString();
     secondsString = (seconds < 10) ? '0' + seconds : seconds.toString();
-    return hoursString + ':' + minutesString + ':' + secondsString;
+    return minutesString + ':' + secondsString;
   }
-
+  
 }
